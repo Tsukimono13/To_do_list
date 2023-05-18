c0ec5fbbb7519b151d8d65df8d90be8be2d8f06e
Irina Litvinova <neforsha@yandex.ru>
2023-05-18 23:41:14 +0300

    fix: refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { store } from "app/store";
 import { Provider } from "react-redux";
 import App from "app/App";
 
+
 const root = createRoot(document.getElementById("root") as HTMLElement);
 root.render(
   <Provider store={store}>
