91911334119b348da04e19fe8a50afaa290610b3
Irina Litvinova <neforsha@yandex.ru>
2023-05-19 00:02:44 +0300

    fix: refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Provider } from "react-redux";
-import App from "app/App";
-import { store } from "app/store";
+import App from "./app/App";
+import { store } from "./app/store";
 
 
 const root = createRoot(document.getElementById("root") as HTMLElement);
